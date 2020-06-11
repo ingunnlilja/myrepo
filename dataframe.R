04343180efeb51f0dece1e158ab2d242808deb82
ingunnlilja <ilb8@hi.is>
2020-06-11 15:28:50 +0000

    Yfirferð á ramma kóða

--- dataframe.R
+++ dataframe.R
@@ -11,7 +11,7 @@ con<-src_mysql("tw_quizdb",
                 user = "ilb8", 
                 password = "AIAtKDEMe2S0I")
 
-# connect
+# Töflur
 answer<-tbl(con,"answer")
 lecture<-tbl(con,"lecture")
 question<-tbl(con,"question")
@@ -20,27 +20,35 @@ lectureStudentSetting<-tbl(con,"lectureStudentSetting")
 
 read_csv('data/cue_all.txt', col_names = F) %>% pull() -> cue_vec
 
+#Fer í gegnum question töfluna og vel út þau plonepath sem matcha við cue_vec
+#Safna þeim saman og svo vel ég úr questionId og plonepath og bý til, cue_id_vec
+#Er þá komin með töflu sem inniheldur id og plonepath
 question %>%
   filter(plonePath %in% cue_vec) %>%
   collect() %>%
   dplyr::select(questionId, plonePath) -> cue_id_vec
 
+#Vel úr answer töflunni sameiginleg gildi á milli ID - safnað saman í cue_tmp
 answer %>%
   filter(questionId %in% local(cue_id_vec$questionId)) %>%
   collect() -> cue_tmp
 
+#Vel lectureId úr cue_tmp og geri einstakt
 cue_lecture <- unique(cue_tmp$lectureId)
 
+#Fer svo í gegnum lecture með lectureID úr cue
 lecture %>%
   filter(lectureId %in% local(cue_lecture)) %>% 
   collect()
 
+#aftur í answer töfluna 
 answer %>%
-  #group_by(lectureId, studentId) %>%
+  group_by(lectureId, studentId) %>%
   filter(lectureId %in% local(cue_lecture)) %>%
   collect() %>%
   group_by(lectureId, studentId) %>%
   filter(any(questionId %in% local(cue_id_vec$questionId))) -> answer_cue_full
+
 
 write_csv(x = answer_cue_full, path = 'data/answer_cue_full.csv')
 
