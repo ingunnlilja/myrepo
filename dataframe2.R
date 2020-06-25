5d982761ec55c71b72b968af9c287d6bf1990a12
ingunnlilja <ilb8@hi.is>
2020-06-25 13:13:40 +0000

    Ramminn til vol1000

--- dataframe2.R
+++ dataframe2.R
@@ -10,34 +10,42 @@ con<-src_mysql("tw_quizdb",
                port = 3306, 
                user = "ilb8", 
                password = "AIAtKDEMe2S0I")
-# connect
+
+
 answer<-tbl(con,"answer")
 lecture<-tbl(con,"lecture")
 question<-tbl(con,"question")
 subscript <- tbl(con, 'subscription')
 lectureStudentSetting<-tbl(con,"lectureStudentSetting")
 
+
 read_csv('data/cue_all.txt', col_names = F) %>% pull() -> cue_vec
+
 
 read_csv('data/cue_all.txt', col_names = F) %>%
   separate(X1, into = c('plonePath', 'cue'), sep = '.cue') %>%
   pull(plonePath) -> nocue_vec
+
 
 question %>%
   filter(plonePath %in% cue_vec) %>%
   collect() %>%
   dplyr::select(questionId, plonePath) -> cue_id_vec
 
+
 question %>%
   filter(plonePath %in% nocue_vec) %>%
   collect() %>%
   dplyr::select(questionId, plonePath) -> nocue_id_vec
 
+
 intersect(nocue_id_vec$questionId, cue_id_vec$questionId)
+
 
 answer %>%
   filter(questionId %in% local(cue_id_vec$questionId)) %>%
   collect() -> cue_tmp
+
 
 answer %>%
   filter(questionId %in% local(nocue_id_vec$questionId)) %>%
@@ -54,5 +62,22 @@ cue_tmp %>%
   mutate(cue = 1) -> tmp2
 
 rbind(tmp1, tmp2) -> ans
+
+tmp3 <- (strsplit(ans$plonePath, "/"))
+
+df <- data.frame(matrix(unlist(tmp3), nrow=length(tmp3), byrow=T))
+
+names(df)
+
+glimpse(df)
+
+df %>% 
+  dplyr::select(X6) -> qName
+
+bind_cols(ans, qName) -> ans
+
+names(ans) <- c("answerId", "lectureId", "studentId", "questionId", "plonePath", "chosenAnswer", "grade", "correct", "timeStart",
+                "timeEnd", "practice", "coinsAwarded", "ugQuestionGuid", "lectureVersion", "cue", "qName")
+
 
 write_csv(x = ans, path = 'data/answers_cue_nocue.csv')
